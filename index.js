ebba418689aecadff8e5ccc480ceff731b97c148
leonardotadeufss <leonardotadeusg@gmail.com>
2021-02-21 02:51:20 -0300

    Add Complete Task function

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function addNewTask(e) {
         name.className = "task name";
         btn.className = "task delete";
         
-        check.innerHTML = `<input type="checkbox" class="check-task">`;
+        check.innerHTML = `<input type="checkbox" class="check-task" onchange="completetask(this.parentElement)">`;
         name.innerText = input.value;
         btn.innerHTML = `   <button class="btnDelete" onclick="removeTask(this.parentElement)">
                                 <svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
@@ -74,4 +74,11 @@ function removeTask(object) {
 function updateTotal() {
     let itens = document.querySelector('.itemleft');
     itens.innerText = `${tablebody.rows.length-1} itens restantes`;
+}
+
+function completetask(object) {
+   let taskname = object.nextSibling
+   object.firstChild.classList.toggle("-completed")
+   taskname.classList.toggle("-completed")
+   
 }
