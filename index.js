22e2b9c7b9f60804139f47df7a6359a47a8732a6
leonardotadeufss <leonardotadeusg@gmail.com>
2021-02-21 00:51:19 -0300

    Add the Night mode

---
+++ index.js
@@ -0,0 +1,25 @@
+const nightIcon = document.querySelector('.nighticon');
+const body = document.querySelector('.body')
+const input = document.querySelector('.input-txt')
+const table = document.querySelector('.todo-table')
+const deleteButton = document.querySelector('.btnDelete')
+
+
+body.style.transition = "all 0.8s";
+input.style.transition = "all 0.5s";
+table.style.transition = "all 0.5s";
+deleteButton.style.transition = "all 0.5s";
+
+nightIcon.addEventListener("click", changeMode);
+
+
+
+
+
+function changeMode() {
+    nightIcon.classList.toggle("-night")
+    body.classList.toggle("-night");
+    input.classList.toggle("-night");
+    table.classList.toggle("-night")
+    deleteButton.classList.toggle("-night")
+}
