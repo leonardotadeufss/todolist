1c6b8259abec31f769831229c4613a0ff9681dd9
leonardotadeufss <leonardotadeusg@gmail.com>
2021-02-25 00:29:53 -0300

    Add responsive layout

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const mainAction = document.querySelectorAll('.main-action')
 var deleteButton = document.querySelectorAll('.btnDelete')
 
 
-
 body.style.transition = "all 0.5s";
 input.style.transition = "all 0.5s";
 todoTable.style.transition = "all 0.5s";
+tableFooter.style.transition = "all 0.5s";
 
 nightIcon.addEventListener("click", changeMode);
 input.addEventListener('keypress', function(e) {
@@ -41,6 +41,7 @@ function changeMode() {
     input.classList.toggle("-night");
     todoTable.classList.toggle("-night")
     bgImage.classList.toggle("-night")
+    tableFooter.classList.toggle("-night")
 }
 
 function addNewTask(e) {
@@ -56,12 +57,17 @@ function addNewTask(e) {
         name.className = "task name";
         btn.className = "task delete";
         
+        
         check.innerHTML = `<input type="checkbox" class="check-task" onchange="completetask(this.parentElement)">`;
         name.innerText = input.value;
         btn.innerHTML = `   <button class="btnDelete" onclick="removeTask(this.parentElement)">
                                 <svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
                              </button>`
-        
+
+        if (tableFooter.classList.contains('-night')) {
+            btn.lastChild.classList.add('-night')
+            
+        }
         row.appendChild(check);
         row.appendChild(name);
         row.appendChild(btn);
